289972a3deec51706975359127379c00df11520e
Lullaby16 <samuelys945@gmail.com>
2022-03-18 19:20:02 +0700

    update score screen

--- src/components/EndScreen/EndScreen.js
+++ src/components/EndScreen/EndScreen.js
@@ -1,6 +1,8 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { colors } from '../../constants'
+import { StyleSheet, Text, View, Pressable, Alert } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { colors, colorsToEmoji } from '../../constants'
+import Clipboard from '@react-native-community/clipboard'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const Number = ({ number, label }) => (
     <View style={{ alignItems: "center", margin: 10}}>
@@ -9,16 +11,23 @@ const Number = ({ number, label }) => (
     </View>
 )
 
-const GuessDistributionLine = ({ position, amount }) => {
+const GuessDistributionLine = ({ position, amount, percentage }) => {
     return (
-        <View style={{ flexDirection: "row", alignItems: "center", width: "100%"}}>
+        <View 
+            style={{ 
+                flexDirection: "row", 
+                alignItems: "center", 
+                width: "100%"
+            }}
+        >
             <Text style={{color: colors.lightgrey}}>{position}</Text>
             <View 
                 style={{
                     alignSelf: "stretch",
                     backgroundColor: colors.grey, 
                     margin: 5, 
-                    padding: 5
+                    padding: 5,
+                    width: `${percentage}%`
                 }}
             >
                 <Text style={{color: colors.lightgrey}}>{amount}</Text>
@@ -27,25 +36,141 @@ const GuessDistributionLine = ({ position, amount }) => {
     )
 }
 
-const EndScreen = ({ menang = false }) => {
-  return (
-    <View>
-      <Text style={styles.title}>
-          {menang ? "Selamat kamu menang" : "Kamu kalah, coba lagi besok"}
-      </Text>
-      <Text style={styles.subtitle}>STATISTICS</Text>
-      <View style={{flexDirection: "row", marginBottom: 20}}>
-        <Number number={2} label={"Played"}/>
-        <Number number={2} label={"Win %"}/>
-        <Number number={2} label={"Cur streak"}/>
-        <Number number={2} label={"Max streak"}/>
-      </View>
-    <Text style={styles.subtitle}>GUESS DISTRIBUTION</Text>
-    <View>
-        <GuessDistributionLine position={0} amount={2} />
-    </View>
-    </View>
-  )
+const GuessDistribution = () => {
+    return (
+        <>
+            <Text style={styles.subtitle}>GUESS DISTRIBUTION</Text>
+            <View style={{ width: "100%", padding: 20, }}>
+                <GuessDistributionLine position={0} amount={2} percentage={50}/>
+            </View>
+        </>
+    )
+}
+
+const EndScreen = ({ menang = false, rows, getCellBGColor }) => {
+    const [second, setSecond] = useState(0)
+    const [played, setPlayed] = useState(0)
+    const [winRate, setWinRate] = useState(0)
+    const [curStreak, setCurStreak] = useState(0)
+    const [maxStreak, setMaxStreak] = useState(0)
+
+    useEffect(() => {
+        readState()
+    }, [])
+
+    useEffect(() => {
+        const updateTime = () => {
+            const now = new Date()
+            const tomorrow = new Date(
+                now.getFullYear(), 
+                now.getMonth(), 
+                now.getDate() + 1
+            )
+        
+            setSecond((tomorrow - now) / 1000)
+        }
+
+        const interval = setInterval(updateTime, 1000)
+        return () => clearInterval(interval)
+    }, [])
+
+    const readState = async () => {
+        const dataString = await AsyncStorage.getItem("@game")
+        let data
+        try {
+            data = JSON.parse(dataString)
+            console.log(data)
+        } catch(e) {
+            console.log("Couldn't parse the state")
+        }
+
+        const keys = Object.keys(data)
+        const values = Object.values(data)
+
+        setPlayed(Object.keys(data).length)
+
+        const numberOfWin = values.filter(
+            game => game.gameState === "menang"
+        ).length
+        setWinRate(Math.floor((100 * numberOfWin) / keys.length))
+        
+        let _curStreak = 0
+        let maxStreak = 0
+        let prevDay = 0
+        keys.forEach((key) => {
+            const day = parseInt(key.split("-")[1])
+            if (data[key].gameState === "menang" && _curStreak === 0) {
+                _curStreak += 1
+            } else if (data[key].gameState === "menang" && prevDay + 1 === day) {
+                _curStreak += 1
+            } else {
+                if (_curStreak > maxStreak) {
+                    maxStreak = _curStreak
+                }
+                _curStreak = data[key].gameState === "menang" ? 1 : 0
+            }
+            prevDay = day
+        }) 
+        setCurStreak(_curStreak)  
+        setMaxStreak(_curStreak)         
+    }
+
+    const share = () => {
+        const textMap = rows.map((row, i) =>
+            row.map((cell, j) => colorsToEmoji[getCellBGColor(i, j)]).join("")
+            ).filter((row) => row)
+            .join("\n")
+        
+        const textToShare = `SusunKatla \n ${textMap}`
+        Clipboard.setString(textToShare)
+        Alert.alert("Copied successfully", "Share your score on your social media")
+    }
+
+    const formatSeconds = () => {
+        const hours = Math.floor(second / (60 * 60))
+        const minute = Math.floor((second % (60 * 60)) / 60)
+        const seconds = Math.floor(second % 60)
+
+        return `${hours}:${minute}:${seconds}`
+    }
+
+    return (
+        <View style={{ width: "100%", alignItems: "center" }}>
+        <Text style={styles.title}>
+            {menang ? "Selamat kamu menang" : "Kamu kalah, coba lagi besok"}
+        </Text>
+
+        <Text style={styles.subtitle}>STATISTICS</Text>
+        <View style={{flexDirection: "row", marginBottom: 20}}>
+            <Number number={played} label={"Played"}/>
+            <Number number={winRate} label={"Win %"}/>
+            <Number number={curStreak} label={"Cur streak"}/>
+            <Number number={maxStreak} label={"Max streak"}/>
+        </View>
+
+        <GuessDistribution />
+
+        <View style={{ flexDirection: "row", padding: 10}}>
+            <View style={{ alignItems: "center", flex: 1 }}>
+                <Text style={{ color: colors.lightgrey }}>Next Katla</Text>
+                <Text style={{ color: colors.lightgrey, fontSize: 24, fontWeight: "bold" }}>{formatSeconds()}</Text>
+            </View>
+
+            <Pressable 
+                onPress={share} 
+                style={{ 
+                    flex: 1, 
+                    backgroundColor: colors.primary, 
+                    borderRadius: 25, 
+                    alignItems: "center", 
+                    justifyContent: "center" 
+                }}
+            >
+                <Text style={{ color: colors.lightgrey, fontWeight: "bold"}}>Share</Text>
+            </Pressable>
+        </View>
+        </View>
+    )
 }
 
 export default EndScreen
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,201 +1,178 @@
-import React, { useState, useEffect } from 'react'
-import { Text, View, ScrollView, Alert, ActivityIndicator} from 'react-native'
-import { colors, CLEAR, ENTER, colorsToEmoji } from '../../constants'
-import Keyboard from '../Keyboard'
-import Clipboard from '@react-native-community/clipboard'
-import words from '../../words'
-import styles from './Game.styles'
-import { copyArray, getDayOfTheYear, getDayKey } from '../utils'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import EndScreen from '../EndScreen'
+import React, { useState, useEffect } from 'react';
+import { Text, View, ScrollView, ActivityIndicator } from 'react-native';
+import { colors, CLEAR, ENTER } from '../../constants';
+import Keyboard from '../Keyboard';
+import words from '../../words';
+import styles from './Game.styles';
+import { copyArray, getDayOfTheYear, getDayKey } from '../utils';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import EndScreen from '../EndScreen';
 
-const NUMBER_OF_TRIES = 6
+const NUMBER_OF_TRIES = 6;
 
-const dayOfTheYear = getDayOfTheYear()
-const dayKey = getDayKey()
+const dayOfTheYear = getDayOfTheYear();
+const dayKey = getDayKey();
 
 const Game = () => {
   // AsyncStorage.removeItem("@game")
-  const word = words[dayOfTheYear]
-  const letters = word.split('') // ['h', 'e', 'l', 'l', 'o']
+  const word = words[dayOfTheYear];
+  const letters = word.split(''); // ['h', 'e', 'l', 'l', 'o']
 
-  const [rows, setRows] = useState(new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill("")))
-  const [curRow, setCurRow] = useState(0)
-  const [curCol, setCurCol] = useState(0)
-  const [gameState, setGameState] = useState('bermain')
-  const [loaded, setLoaded] = useState(false)
+  const [rows, setRows] = useState(
+    new Array(NUMBER_OF_TRIES).fill(new Array(letters.length).fill('')),
+  );
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+  const [gameState, setGameState] = useState('bermain');
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     if (curRow > 0) {
-      checkGameState()
+      checkGameState();
     }
-  }, [curRow])
+  }, [curRow]);
 
   useEffect(() => {
     if (loaded) {
-      persisState()
+      persisState();
     }
-  }, [rows, curRow, curCol, gameState])
+  }, [rows, curRow, curCol, gameState]);
 
   useEffect(() => {
-    readState()
-  }, [])
+    readState();
+  }, []);
 
   const persisState = async () => {
-      // write all the state variables in async storage
-      const dataForToday = {
-          rows,
-          curCol,
-          curRow,
-          gameState,
-      }
+    // write all the state variables in async storage
+    const dataForToday = {
+      rows,
+      curCol,
+      curRow,
+      gameState,
+    };
 
-      try {
-        const existingStateString = await AsyncStorage.getItem("@game")
-        const existingState = existingStateString 
-          ? JSON.parse(existingStateString) 
-          : {}
+    try {
+      const existingStateString = await AsyncStorage.getItem('@game');
+      const existingState = existingStateString
+        ? JSON.parse(existingStateString)
+        : {};
 
-        existingState[dayKey] = dataForToday
+      existingState[dayKey] = dataForToday;
 
-        const dataString = JSON.stringify(existingState)
-        await AsyncStorage.setItem("@game", dataString)
-      } catch(e) {
-        console.log("Failed to write data to async storage", e)
-      }
-  }
+      const dataString = JSON.stringify(existingState);
+      await AsyncStorage.setItem('@game', dataString);
+    } catch (e) {
+      console.log('Failed to write data to async storage', e);
+    }
+  };
 
   const readState = async () => {
-      const dataString = await AsyncStorage.getItem("@game")
-      try {
-        const data = JSON.parse(dataString)
-        const day = data[dayKey]
-        setRows(day.rows)
-        setCurCol(day.curCol)
-        setCurRow(day.curRow)
-        setGameState(day.gameState)
-      } catch(e) {
-          console.log("Couldn't parse the state")
-      }
+    const dataString = await AsyncStorage.getItem('@game');
+    try {
+      const data = JSON.parse(dataString);
+      const day = data[dayKey];
+      setRows(day.rows);
+      setCurCol(day.curCol);
+      setCurRow(day.curRow);
+      setGameState(day.gameState);
+    } catch (e) {
+      console.log("Couldn't parse the state");
+    }
 
-      setLoaded(true)
-  }
+    setLoaded(true);
+  };
 
   const checkGameState = () => {
     if (checkIfWon() && gameState !== 'menang') {
-      Alert.alert(
-      "Selamat",
-      "Kamu menang",
-      [
-        {
-          text: "Bagikan",
-          onPress: () => shareScore(),
-          style: "cancel"
-        },
-        { 
-          text: "OK", 
-          onPress: () => null
-        }
-      ],
-      { cancelable: true}
-    );
-      setGameState('menang')
+      setGameState('menang');
     } else if (checkIfLost() && gameState !== 'kalah') {
-      Alert.alert("Kamu kalah", "Coba lagi besok")
-      setGameState('kalah')
+      setGameState('kalah');
     }
-  }
-
-  const shareScore = () => {
-    const textMap = rows.map((row, i) =>
-      row.map((cell, j) => colorsToEmoji[getCellBGColor(i, j)]).join("")
-    ).filter((row) => row)
-    .join("\n")
-    
-    const textToShare = `SusunKatla \n ${textMap}`
-    Clipboard.setString(textToShare)
-    Alert.alert("Copied successfully", "Share your score on your social media")
-  }
+  };
 
   const checkIfWon = () => {
-    const row = rows[curRow - 1]
+    const row = rows[curRow - 1];
 
-    return row.every((letter, i) => letter === letters[i])
-  }
+    return row.every((letter, i) => letter === letters[i]);
+  };
 
   const checkIfLost = () => {
-    return !checkIfWon() && curRow === rows.length
-  }
+    return !checkIfWon() && curRow === rows.length;
+  };
 
-  const onKeyPressed = (key) => {
+  const onKeyPressed = key => {
     if (gameState !== 'bermain') {
-      return
+      return;
     }
 
-    const updatedRows = copyArray(rows)
+    const updatedRows = copyArray(rows);
 
     if (key === CLEAR) {
-      const prevCol = curCol - 1
+      const prevCol = curCol - 1;
       if (prevCol >= 0) {
-        updatedRows[curRow][prevCol] = ""
-        setRows(updatedRows)
-        setCurCol(prevCol)
+        updatedRows[curRow][prevCol] = '';
+        setRows(updatedRows);
+        setCurCol(prevCol);
       }
-      return 
+      return;
     }
 
     if (key === ENTER) {
       if (curCol === rows[0].length) {
-        setCurRow(curRow + 1)
-        setCurCol(0)
+        setCurRow(curRow + 1);
+        setCurCol(0);
       }
-      return
+      return;
     }
 
     if (curCol < rows[0].length) {
-      updatedRows[curRow][curCol] = key
-      setRows(updatedRows)
-      setCurCol(curCol + 1)
+      updatedRows[curRow][curCol] = key;
+      setRows(updatedRows);
+      setCurCol(curCol + 1);
     }
-  }
+  };
 
   const isCellActive = (row, col) => {
-    return row === curRow && col === curCol
-  }
+    return row === curRow && col === curCol;
+  };
 
-  const getCellBGColor = (row , col) => {
-    const letter = rows[row][col]
+  const getCellBGColor = (row, col) => {
+    const letter = rows[row][col];
 
     if (row >= curRow) {
-      return colors.black
+      return colors.black;
     }
     if (letter === letters[col]) {
-      return colors.primary
+      return colors.primary;
     }
     if (letters.includes(letter)) {
-      return colors.secondary
+      return colors.secondary;
     }
-    return colors.darkgrey
-  }
+    return colors.darkgrey;
+  };
 
-  const getAllLettersWithColor = (color) => {
-    return rows.flatMap((row, i) => 
-      row.filter((cell, j) => getCellBGColor(i, j) === color)
-    )
-  }
+  const getAllLettersWithColor = color => {
+    return rows.flatMap((row, i) =>
+      row.filter((cell, j) => getCellBGColor(i, j) === color),
+    );
+  };
 
-  const greenCaps = getAllLettersWithColor(colors.primary)
-  const yellowCaps = getAllLettersWithColor(colors.secondary)
-  const greyCaps = getAllLettersWithColor(colors.darkgrey)
+  const greenCaps = getAllLettersWithColor(colors.primary);
+  const yellowCaps = getAllLettersWithColor(colors.secondary);
+  const greyCaps = getAllLettersWithColor(colors.darkgrey);
 
   if (!loaded) {
-      return <ActivityIndicator />
+    return <ActivityIndicator />;
   }
 
-  if (gameState !== "bermain") {
+  if (gameState !== 'bermain') {
     return (
-      <EndScreen menang={ gameState === "menang" } />
-    )
+      <EndScreen
+        menang={gameState === 'menang'}
+        rows={rows}
+        getCellBGColor={getCellBGColor}
+      />
+    );
   }
 
   return (
@@ -204,35 +181,32 @@ const Game = () => {
         {rows.map((row, i) => (
           <View key={`row-${i}`} style={styles.row}>
             {row.map((letter, j) => (
-              <View 
+              <View
                 key={`cell-${i}-${j}`}
                 style={[
-                  styles.cell, 
-                  { 
-                    borderColor: isCellActive(i, j) 
-                      ? colors.lightgrey 
+                  styles.cell,
+                  {
+                    borderColor: isCellActive(i, j)
+                      ? colors.lightgrey
                       : colors.darkgrey,
-                    backgroundColor: getCellBGColor(i, j)
-                  }
-                ]}
-              >
+                    backgroundColor: getCellBGColor(i, j),
+                  },
+                ]}>
                 <Text style={styles.cellText}>{letter.toUpperCase()}</Text>
               </View>
             ))}
-          </View>  
+          </View>
         ))}
       </ScrollView>
 
-      <Keyboard 
+      <Keyboard
         onKeyPressed={onKeyPressed}
         greenCaps={greenCaps}
         yellowCaps={yellowCaps}
         greyCaps={greyCaps}
       />
+    </>
+  );
+};
 
-    </>            
-  )
-}
-
-export default Game
-
+export default Game;
