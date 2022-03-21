af814fceb7b0338e481652f28fd24c31efd99285
Lullaby16 <samuelys945@gmail.com>
2022-03-21 11:14:09 +0700

    update animation

--- src/components/EndScreen/EndScreen.js
+++ src/components/EndScreen/EndScreen.js
@@ -5,12 +5,12 @@ import {
   Pressable,
   Alert,
   ScrollView,
-} from 'react-native';
-import React, { useState, useEffect } from 'react';
-import { colors, colorsToEmoji } from '../../constants';
-import Clipboard from '@react-native-community/clipboard';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import Animated, { SlideInLeft } from 'react-native-reanimated';
+} from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { colors, colorsToEmoji } from '../../constants'
+import Clipboard from '@react-native-community/clipboard'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import Animated, { SlideInLeft } from 'react-native-reanimated'
 
 const Number = ({ number, label }) => (
   <View style={{ alignItems: 'center', margin: 10 }}>
@@ -19,7 +19,7 @@ const Number = ({ number, label }) => (
     </Text>
     <Text style={{ color: colors.lightgrey, fontSize: 16 }}>{label}</Text>
   </View>
-);
+)
 
 const GuessDistributionLine = ({ position, amount, percentage }) => {
   return (
@@ -42,14 +42,14 @@ const GuessDistributionLine = ({ position, amount, percentage }) => {
         <Text style={{ color: colors.lightgrey }}>{amount}</Text>
       </View>
     </View>
-  );
-};
+  )
+}
 
 const GuessDistribution = ({ distribution }) => {
   if (!distribution) {
-    return null;
+    return null
   }
-  const sum = distribution.reduce((dist, total) => dist + total, 0);
+  const sum = distribution.reduce((dist, total) => dist + total, 0)
   return (
     <>
       <Text style={styles.subtitle}>GUESS DISTRIBUTION</Text>
@@ -64,89 +64,89 @@ const GuessDistribution = ({ distribution }) => {
         ))}
       </View>
     </>
-  );
-};
+  )
+}
 
 const EndScreen = ({ menang = false, rows, getCellBGColor }) => {
-  const [second, setSecond] = useState(0);
-  const [played, setPlayed] = useState(0);
-  const [winRate, setWinRate] = useState(0);
-  const [curStreak, setCurStreak] = useState(0);
-  const [maxStreak, setMaxStreak] = useState(0);
-  const [distribution, setDistribution] = useState(null);
+  const [second, setSecond] = useState(0)
+  const [played, setPlayed] = useState(0)
+  const [winRate, setWinRate] = useState(0)
+  const [curStreak, setCurStreak] = useState(0)
+  const [maxStreak, setMaxStreak] = useState(0)
+  const [distribution, setDistribution] = useState(null)
 
   useEffect(() => {
-    readState();
-  }, []);
+    readState()
+  }, [])
 
   useEffect(() => {
     const updateTime = () => {
-      const now = new Date();
+      const now = new Date()
       const tomorrow = new Date(
         now.getFullYear(),
         now.getMonth(),
         now.getDate() + 1,
-      );
+      )
 
-      setSecond((tomorrow - now) / 1000);
-    };
-
-    const interval = setInterval(updateTime, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const readState = async () => {
-    const dataString = await AsyncStorage.getItem('@game');
-    let data;
-    try {
-      data = JSON.parse(dataString);
-      console.log(data);
-    } catch (e) {
-      console.log("Couldn't parse the state");
+      setSecond((tomorrow - now) / 1000)
     }
 
-    const keys = Object.keys(data);
-    const values = Object.values(data);
+    const interval = setInterval(updateTime, 1000)
+    return () => clearInterval(interval)
+  }, [])
 
-    setPlayed(Object.keys(data).length);
+  const readState = async () => {
+    const dataString = await AsyncStorage.getItem('@game')
+    let data
+    try {
+      data = JSON.parse(dataString)
+      console.log(data)
+    } catch (e) {
+      console.log("Couldn't parse the state")
+    }
+
+    const keys = Object.keys(data)
+    const values = Object.values(data)
+
+    setPlayed(Object.keys(data).length)
 
     const numberOfWin = values.filter(
       game => game.gameState === 'menang',
-    ).length;
-    setWinRate(Math.floor((100 * numberOfWin) / keys.length));
+    ).length
+    setWinRate(Math.floor((100 * numberOfWin) / keys.length))
 
-    let _curStreak = 0;
-    let maxStreak = 0;
-    let prevDay = 0;
+    let _curStreak = 0
+    let maxStreak = 0
+    let prevDay = 0
     keys.forEach(key => {
-      const day = parseInt(key.split('-')[1]);
+      const day = parseInt(key.split('-')[1])
       if (data[key].gameState === 'menang' && _curStreak === 0) {
-        _curStreak += 1;
+        _curStreak += 1
       } else if (data[key].gameState === 'menang' && prevDay + 1 === day) {
-        _curStreak += 1;
+        _curStreak += 1
       } else {
         if (_curStreak > maxStreak) {
-          maxStreak = _curStreak;
+          maxStreak = _curStreak
         }
-        _curStreak = data[key].gameState === 'menang' ? 1 : 0;
+        _curStreak = data[key].gameState === 'menang' ? 1 : 0
       }
-      prevDay = day;
-    });
-    setCurStreak(_curStreak);
-    setMaxStreak(_curStreak);
+      prevDay = day
+    })
+    setCurStreak(_curStreak)
+    setMaxStreak(_curStreak)
 
     // guess distribution
 
-    const dist = [0, 0, 0, 0, 0, 0];
+    const dist = [0, 0, 0, 0, 0, 0]
 
     values.map(game => {
       if (game.gameState === 'menang') {
-        const tries = game.rows.filter(row => row[0]).length;
-        dist[tries] = dist[tries] + 1;
+        const tries = game.rows.filter(row => row[0]).length
+        dist[tries] = dist[tries] + 1
       }
-    });
-    setDistribution(dist);
-  };
+    })
+    setDistribution(dist)
+  }
 
   const share = () => {
     const textMap = rows
@@ -154,20 +154,20 @@ const EndScreen = ({ menang = false, rows, getCellBGColor }) => {
         row.map((cell, j) => colorsToEmoji[getCellBGColor(i, j)]).join(''),
       )
       .filter(row => row)
-      .join('\n');
+      .join('\n')
 
-    const textToShare = `SusunKatla \n ${textMap}`;
-    Clipboard.setString(textToShare);
-    Alert.alert('Copied successfully', 'Share your score on your social media');
-  };
+    const textToShare = `SusunKatla \n ${textMap}`
+    Clipboard.setString(textToShare)
+    Alert.alert('Copied successfully', 'Share your score on your social media')
+  }
 
   const formatSeconds = () => {
-    const hours = Math.floor(second / (60 * 60));
-    const minute = Math.floor((second % (60 * 60)) / 60);
-    const seconds = Math.floor(second % 60);
+    const hours = Math.floor(second / (60 * 60))
+    const minute = Math.floor((second % (60 * 60)) / 60)
+    const seconds = Math.floor(second % 60)
 
-    return `${hours}:${minute}:${seconds}`;
-  };
+    return `${hours}:${minute}:${seconds}`
+  }
 
   return (
     <ScrollView
@@ -218,16 +218,22 @@ const EndScreen = ({ menang = false, rows, getCellBGColor }) => {
             alignItems: 'center',
             justifyContent: 'center',
           }}>
-          <Text style={{ color: colors.lightgrey, fontWeight: 'bold' }}>
-            Share
+          <Text
+            style={{
+              color: colors.lightgrey,
+              fontWeight: 'bold',
+              fontSize: 18,
+              letterSpacing: 2,
+            }}>
+            SHARE
           </Text>
         </Pressable>
       </Animated.View>
     </ScrollView>
-  );
-};
+  )
+}
 
-export default EndScreen;
+export default EndScreen
 
 const styles = StyleSheet.create({
   title: {
@@ -243,4 +249,4 @@ const styles = StyleSheet.create({
     marginVertical: 5,
     fontWeight: 'bold',
   },
-});
+})
